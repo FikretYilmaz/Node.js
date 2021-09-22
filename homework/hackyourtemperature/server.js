0c3d6155526693727a374b588d56b89d6becd8dd
Fikret Yilmaz <yilmz.fikret@gmail.com>
2021-09-22 18:47:39 +0200

    FIkret Yilmaz w1 NodeJS

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
-//import bodyParser from 'body-parser';
 
 const app = express();
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 app.use(express.json());
-//app.use(bodyParser.json());
+
 app.post('/weather/:cityName', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify('cityName:' + req.params.cityName));
