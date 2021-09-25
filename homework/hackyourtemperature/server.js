2436f57ded9904286848442d3db32680a6d14480
Fikret Yilmaz <yilmz.fikret@gmail.com>
2021-09-25 07:46:50 +0200

    FIKRET YILMAZ NODEJS_W1 Update

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -4,12 +4,15 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
+
 app.use(express.json());
 
 app.post('/weather/:cityName', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify('cityName:' + req.params.cityName));
+  const cityName = { 'City Name:': req.params.cityName };
+  res.send(cityName);
 });
+
 app.listen(3000, () => {
   console.log(`app is listening to port 3000`);
 });
